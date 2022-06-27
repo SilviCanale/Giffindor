cf75db96347972ff62cdf9648f47034694e3e11c
josemarza <josemarzamendia@gmail.com>
2022-06-27 11:28:03 -0400

    Generar la función de creación de base de datos

--- app.py
+++ app.py
@@ -1,6 +1,20 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from config import config
+from models import db, Residente_envia, Residente_recibe, Involucrado
 import requests
-app = Flask(__name__)
+
+def createapp(environment):
+    app = Flask(__name__)
+    app.config.from_object(environment)
+    with app.app_context(): 
+        db.init_app(app)
+        db.create_all()
+    return app
+
+environment=config["development"]
+
+app=createapp(environment)
 
 @app.route("/")
 def primer_challenge():
